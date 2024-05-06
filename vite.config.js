e1a919459b2c5fecc1323cea1decdcebfb082a18
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-05-06 23:12:33 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        contacts: resolve(__dirname, 'contacts.html'),
+        // contacts: resolve(__dirname, 'contacts.html'),
       },
     },
   },

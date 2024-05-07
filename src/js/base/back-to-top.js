d2066626536b21c02254269f334a13444acbdd23
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-05-07 14:39:05 +0300

    fix_b-t-t

--- src/js/base/back-to-top.js
+++ src/js/base/back-to-top.js
@@ -1,7 +1,9 @@
 import { addBackToTop } from 'vanilla-back-to-top';
 
+const sizeBackToTop = () => (window.innerWidth > 768 ? 50 : 40);
+
 addBackToTop({
-  diameter: 40,
+  diameter: sizeBackToTop(),
   backgroundColor: '#333333',
   textColor: '#ffffff',
   scrollDuration: 400,

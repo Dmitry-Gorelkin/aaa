dba13306171696ed5c910054134dac18c58a1193
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-05-08 21:05:09 +0300

    fix

--- src/js/modules/mobMenu.js
+++ src/js/modules/mobMenu.js
@@ -4,12 +4,18 @@ const { headerMenu, headerOpenMenu, headerCloseMenu } = refs;
 
 const openMenu = () => {
   headerMenu.classList.remove('is-hidden');
+
   headerCloseMenu.addEventListener('click', closeMenu);
+
+  document.body.style.overflow = 'hidden';
 };
 
 const closeMenu = () => {
   headerMenu.classList.add('is-hidden');
+
   headerCloseMenu.removeEventListener('click', closeMenu);
+
+  document.body.style.overflow = 'auto';
 };
 
 headerOpenMenu.addEventListener('click', openMenu);
